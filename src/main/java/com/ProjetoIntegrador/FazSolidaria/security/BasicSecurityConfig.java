36b7a7c56f2d6ac253a482371b75aecafdb62d01
gerson espejo <gersonespejo127@gmail.com>
2022-07-08 19:55:07 -0300

    .

--- src/main/java/com/ProjetoIntegrador/FazSolidaria/security/BasicSecurityConfig.java
+++ src/main/java/com/ProjetoIntegrador/FazSolidaria/security/BasicSecurityConfig.java
@@ -35,6 +35,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+		.antMatchers("/**").permitAll()
 		.antMatchers("/usuarios/login-usuario").permitAll()
 		.antMatchers("/usuarios/cadastrar-usuario").permitAll()
 		.antMatchers("/produtos").permitAll()
